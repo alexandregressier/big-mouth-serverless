6bd38e71564add22b7de362a197bee879743070b
Alexandre Gressier <alexandre@gressier.dev>
2021-12-06 17:31:44 +0100

    Initial revision of Big Mouth Serverless

---
+++ src/functions/hello/handler.ts
@@ -0,0 +1,13 @@
+import type { ValidatedEventAPIGatewayProxyEvent } from "@libs/apiGateway"
+import { formatJSONResponse } from "@libs/apiGateway"
+import { middyfy } from "@libs/lambda"
+
+import schema from "./schema"
+
+const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) =>
+    formatJSONResponse({
+        message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
+        event,
+    })
+
+export const main = middyfy(hello)
---
+++ src/libs/lambda.ts
@@ -0,0 +1,5 @@
+import middy from "@middy/core"
+import middyJsonBodyParser from "@middy/http-json-body-parser"
+
+export const middyfy = (handler) =>
+    middy(handler).use(middyJsonBodyParser())
